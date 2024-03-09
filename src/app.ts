8e2069525d078cbcfc8af182ab88332e41ad253b
jesintharnold <jesintharnold@gmail.com>
2024-03-09 21:28:26 +0530

    2

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,27 @@ class ProjectInput{
     templateElement:HTMLTemplateElement;
     renderElement:HTMLDivElement;
     formElement:HTMLFormElement;
+
+    title:HTMLInputElement;
+    description:HTMLInputElement;
+    people:HTMLInputElement;
 constructor(){
     this.templateElement=document.getElementById("project-input")! as HTMLTemplateElement;
     this.renderElement=document.getElementById("app")! as HTMLDivElement;
     const nodes=document.importNode(this.templateElement.content,true);
     this.formElement=nodes.firstElementChild as HTMLFormElement;
+    this.formElement.id="user-input";
+    this.title=this.formElement.querySelector("#title") as HTMLInputElement;
+    this.description=this.formElement.querySelector("#description") as HTMLInputElement;
+    this.people=this.formElement.querySelector("#people") as HTMLInputElement;
+    this.formElement.addEventListener('submit',this.submitHandler.bind(this));
     this.render(this.formElement);
 };
+
+private submitHandler(event:Event){
+    event.preventDefault();
+    console.info(this.title.value,this.description.value,this.people.value);
+};  
 
 private render(form:HTMLFormElement){
     this.renderElement.insertAdjacentElement('afterbegin',form);
